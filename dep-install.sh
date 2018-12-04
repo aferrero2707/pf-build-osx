acfb1040f5400f8d04767a22a591810b23aa5ce5
aferrero2707 <aferrero1975@gmail.com>
2018-12-04 13:43:37 +0100

    Update dep-install.sh

--- dep-install.sh
+++ dep-install.sh
@@ -11,8 +11,10 @@ cachefile=homebrew-cache-pf-step$((step-1)).tar.gz
 cacheurl=https://github.com/aferrero2707/pf-build-osx/releases/download/continuous/$cachefile
 
 #watch -n 5 echo "Still running..."&
-echo 'while :; do clear; echo "Still running..."; sleep 60; done' > "$HOME/watch.sh"
-bash -c "$HOME/watch.sh" &
+echo 'while :; do echo "Still running..."; sleep 60; done' > "$HOME/watch.sh"
+chmod +x "$HOME/watch.sh"
+cat "$HOME/watch.sh"
+bash "$HOME/watch.sh" &
 
 cd $HOME
 if [ x"$step" != "x0" ]; then
